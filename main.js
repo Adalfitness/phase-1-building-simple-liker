9d0be2b5afcbb567ac5f890c36617abb40f3163c
carlos bernal <bernal199152@yahoo.com>
2024-10-20 10:51:56 -0400

    complete lab

--- main.js
+++ main.js
@@ -3,6 +3,46 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const modal = document.getElementById('modal');
+const modalMessage = document.getElementById("modal-message");
+
+modal.classList.add('hidden');
+
+function handleHeartClick(event){
+  const heart = event.target;
+
+mimicServerCall()
+.then(() => {
+  if(heart.innerText === EMPTY_HEART ){
+    heart.innerText = FULL_HEART
+    heart.classList.add('activated-heart');
+  } else {
+    heart.innerText = EMPTY_HEART;
+    heart.classList.remove('activated-heart');
+
+
+  }
+
+})
+.catch((error)=> {
+  modalMessage.innerText = error;
+  modal.classList.remove('hidden');
+
+  setTimeout(() => {
+    modal.classList.add('hidden');
+
+  } , 3000);
+});
+
+
+}
+
+const hearts = document.querySelectorAll(".like-glyph");
+hearts.forEach(heart => {
+  heart.addEventListener('click', handleHeartClick);
+});
+
+
 
 
 
